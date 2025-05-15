b7dad1c754ffdeba7150717164c63c2c60f4bbab
rafaowav <rafal5502@hotmail.com>
2025-05-15 08:25:09 -0300

    incluso mensagem inicial do sistema no Main.java.

--- Main.java
+++ Main.java
@@ -1,8 +1,31 @@
 
+import java.util.Scanner;
+
+
 
 public class Main {
     public static void main(String[] args) {
+       
+        Scanner sc = new Scanner(System.in);
         
+
+        String nome;
+
+        System.out.println("Digite seu nome: ");
+        nome = sc.nextLine();
+        System.out.println();
+        System.out.println("Bem vindo " + nome + "!");
+        System.out.println();
+
+
+            System.out.println("\n--- MENU ---");
+                System.out.println("1. Cadastrar música");
+                System.out.println("2. Listar músicas");
+                System.out.println("3. Avaliar música");
+                System.out.println("4. Ver histórico de avaliações");
+                System.out.println("0. Sair");
+                System.out.println();
+
 
 
 
