590eddcd3a9a5dcb0772386264440896186d8688
matesu <mcos.mateus@gmail.com>
2025-05-15 08:05:56 -0300

    incluso save e track

---
+++ src/track.java
@@ -0,0 +1,11 @@
+package src;
+
+public class track {
+    String nomeMusica;
+    String nomeAlbum;
+    String nomeArtista;
+    String genero;
+    int ano;
+    int nota;
+    String descricao;
+}
