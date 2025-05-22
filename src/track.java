a5c282da599726dcd16b38690bb755da00272f36
matesu <mcos.mateus@gmail.com>
2025-05-22 08:33:24 -0300

    implementação da estrutura básica para salvar, e estrutura de dados das faixas

--- src/track.java
+++ src/track.java
@@ -1,11 +1,37 @@
 package src;
 
+// declara variáveis para uso em vetores dentro de main
+
 public class track {
-    String nomeMusica;
-    String nomeAlbum;
-    String nomeArtista;
-    String genero;
-    int ano;
-    int nota;
-    String descricao;
+    private String faixa;
+    private String artista;
+    private String album;
+    private int ano;
+    private int rate;
+
+    // inicializa as variáveis para uso
+    public track(String faixa, String artista, String album, int ano, int rate) {
+        this.faixa = faixa;
+        this.artista = artista;
+        this.album = album;
+        this.ano = ano;
+        this.rate = rate;
+    }
+
+    // getters, para acessar essas variáveis em main, usar método do getter
+    public String getFaixa(){
+        return faixa;
+    }
+    public String getArtista(){
+        return artista;
+    }
+    public String getAlbum(){
+        return album;
+    }
+    public int getAno(){
+        return ano;
+    }
+    public int getRate(){
+        return rate;
+    }
 }
